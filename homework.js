e8dda8b4a2130e97c63071a762fb61c8badc852d
Jade <Webinar@alans-MacBook-Pro-2.local>
2021-06-18 18:46:20 -0400

    new file created

---
+++ homework.js
@@ -0,0 +1,15 @@
+let evenNumbers = [2,4,6,8,10,12,16]
+let unevenNumbers = [1,3,5,7,9,11,15]
+let mixedNumbers = [0, 1, 6, 7, 3, 14]
+
+function returnEvenNumbers (x){
+
+
+
+    return x.filter(y, function(){
+
+    })
+}
+returnEvenNumbers ()
+
+console.log (returnEvenNumbers(evenNumbers))
